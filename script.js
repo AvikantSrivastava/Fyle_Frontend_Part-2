35bafe389f9c70fc8dea47e8a67ae4201f28248e
Avikant Srivastava <asdavikant@gmail.com>
2021-01-23 03:59:13 +0530

    Multiple cities added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Dropdown of cities
+window.alert = function () {};
 
 $(".dropdown-radio")
   .find("input")
@@ -13,77 +14,35 @@ $(".dropdown-radio")
     dropdown.find("button").text(Selected_city);
   });
 
-//   Populating the table
+var jsonData = [{ meta: { version: 1, type: "test" } }];
 
-// fetch("https://flye-backend-avikant.herokuapp.com/api/branches?q=bangalore&limit=100")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     let tableStructure = `<table id="example" class="table table-striped table-bordered" style="width:100%">
-//         <thead>
-//           <tr>
-//            <th>IFSC</th>
-//            <th>Bank ID</th>
-//            <th>Branch</th>
-//            <th>Address</th>
-//            <th>City</th>
-//            <th>District</th>
-//            <th>State</th>
-//           </tr>
-//         </thead>
-//           <tbody id="tbody"></tbody>
-//         <tfoot>
-//           <tr>
-//            <th>IFSC</th>
-//            <th>Bank ID</th>
-//            <th>Branch</th>
-//            <th>Address</th>
-//            <th>City</th>
-//            <th>District</th>
-//            <th>State</th>
-//           </tr>
-//         </tfoot>
-//         </table>
-//       `;
-//     document.getElementById("result").innerHTML = tableStructure;
-//     let tableBody = document.getElementById("tbody");
-//     for (let i = 0; i < Object.keys(data.branches).length; i++) {
-//       let tr = "";
-//       tr += `<tr>
-//             <td>${data.branches[i].ifsc}</td>
-//             <td>${data.branches[i].bank_id}</td>
-//             <td>${data.branches[i].branch}</td>
-//             <td>${data.branches[i].address}</td>
-//             <td>${data.branches[i].city}</td>
-//             <td>${data.branches[i].district}</td>
-//             <td>${data.branches[i].state}</td>
+jQuery.ajaxSetup({ cache: true });
 
-//           </tr>`;
-//       tableBody.innerHTML += tr;
-//     }
-//   })
-//   .catch((error) => console.error(error));
+function plsdo(city) {
+  jQuery.getJSON(
+    `https://flye-backend-avikant.herokuapp.com/api/branches?q=${city}&limit=null`,
+    function (data) {
+      console.log(data);
+      jsonData = data;
 
-var jsonData = [
-    { "meta": { "version": 1, "type": "test" } }
-];
+      $(`#${city}`).DataTable({
+        data: jsonData.branches,
+        columns: [
+          { data: "ifsc" },
+          { data: "bank_id" },
+          { data: "branch" },
+          { data: "address" },
+          { data: "city" },
+          { data: "district" },
+          { data: "state" },
+        ],
+      });
+    }
+  );
+}
 
-jQuery.ajaxSetup({ cache:true });
-
-jQuery.getJSON('https://flye-backend-avikant.herokuapp.com/api/branches?q=varanasi&limit=null', function(data){
-    console.log(data);
-    jsonData = data;
-
-    $('#result').DataTable({
-        "data": jsonData.branches,
-        "columns": [
-                { "data": "ifsc" },
-            { "data": "bank_id" },
-            { "data": "branch" },
-            { "data": "address" },
-            { "data": "city" },
-            { "data": "district" },
-        { "data": "state" }]
-    });
-});
-
-
+plsdo("bangalore");
+plsdo("delhi");
+plsdo("pune");
+plsdo("kolkata");
+plsdo("mumbai");
